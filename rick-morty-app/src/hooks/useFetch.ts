b497b9153360995d513e76e6d21ac37af5ee7358
Alfredo Diez <adiezpay@ext.unicaja.es>
2025-05-02 08:13:23 +0200

    Add AbortController

--- rick-morty-app/src/hooks/useFetch.ts
+++ rick-morty-app/src/hooks/useFetch.ts
@@ -14,25 +14,23 @@ export function useFetch<T>(url: string) {
   });
 
   useEffect(() => {
-    let isMounted = true;
+    const controller = new AbortController(); // <- Controlador
+    const signal = controller.signal;
 
     const fetchData = async () => {
       try {
         setState((prev) => ({ ...prev, isLoading: true }));
 
-        const response = await fetch(url);
+        const response = await fetch(url, { signal }); // <- Se pasa signal
         if (!response.ok) throw new Error("Network error");
 
         const data = await response.json();
-        if (isMounted) {
-          setState({ data, error: null, isLoading: false });
-        }
+        setState({ data, error: null, isLoading: false });
       } catch (err: unknown) {
-        if (isMounted) {
-          let errorMessage = "Unknown error";
-          if (err instanceof Error) {
-            errorMessage = err.message;
-          }
+        if (err instanceof DOMException && err.name === "AbortError") {
+          console.log("🔁 fetch cancelado por desmontaje del componente");
+        } else {
+          const errorMessage = err instanceof Error ? err.message : "Unknown error";
           setState({ data: null, error: errorMessage, isLoading: false });
         }
       }
@@ -41,7 +39,7 @@ export function useFetch<T>(url: string) {
     fetchData();
 
     return () => {
-      isMounted = false;
+      controller.abort(); // <- Cancelación limpia al desmontar
     };
   }, [url]);
 
